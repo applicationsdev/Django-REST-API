f7cd19d5ce789d23f25bd9f14c191180da547c9f
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-03-22 12:30:23 +0000

    model Product created

--- appdata/models.py
+++ appdata/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    sku = models.CharField(max_length=50)
+    desc = models.CharField(max_length=100)
+    vendor = models.CharField(max_length=30)
+    added = models.DateTimeField(auto_now_add=True)
+    qty = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
