ba46ed63513c9ddf2d8e805e13de469f02fbffc9
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-03-28 13:40:32 +0000

    Product -model to JSON- serializer added

---
+++ appdata/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from . models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = [
+            'sku',
+            'title',
+            'short_desc',
+            'date_added',
+            'instock_qty',
+            'net_price',
+            'cat_id',
+            'subcat_id',
+            'vendor_id',
+        ]
